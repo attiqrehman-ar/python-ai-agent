598b9a43d27e8c6e5fd3455eca078024a47661c9
attiqrehman-ar <attiqrehman130@gmail.com>
2024-12-30 01:47:21 +0500

    Add Vercel URL to CSRF_TRUSTED_ORIGINS and ALLOWED_HOSTS

--- ai_agent/settings.py
+++ ai_agent/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+40yvx0=-1vd$4_7bj^c^dps%^=sl9+=v#@3_rh%39b(_ju@1k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ai-agent-mocha.vercel.app', 'localhost'] 
 
 
 # Application definition
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware', 
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ai_agent.urls'
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
@@ -132,3 +135,12 @@ MEDIA_ROOT = BASE_DIR / "media"
 # settings.py
 LOGIN_URL = 'login'  # Ensure this matches the login path in your urls.py
 # Redirect after admin logout
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ai-agent-mocha.vercel.app',  # Add your Vercel app URL here
+]
+import django_heroku
+django_heroku.settings(locals())
