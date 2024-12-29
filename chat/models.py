9867335f6e39c165c5f1ad9508d06d3ee10036e4
attiqrehman-ar <attiqrehman130@gmail.com>
2024-12-29 18:48:13 +0500

    create modals for both apps and add create urls files also

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UserQuery(models.Model):
+    question = models.TextField()
+    answer = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
